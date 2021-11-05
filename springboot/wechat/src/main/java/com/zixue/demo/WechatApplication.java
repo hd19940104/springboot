60f569291aeca3cdc0db177592b8261a644e3cf3
aidenhou <aidenhou@tencent.com>
2021-11-05 14:18:27 +0800

    新增入库

--- springboot/wechat/src/main/java/com/zixue/demo/WechatApplication.java
+++ springboot/wechat/src/main/java/com/zixue/demo/WechatApplication.java
@@ -1,11 +1,17 @@
 package com.zixue.demo;
 
+import com.zixue.demo.dao.MessageInfoMapper;
+import com.zixue.demo.dto.model.MessageInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.Objects;
 
 @SpringBootApplication
 @Controller
@@ -14,10 +20,17 @@ public class WechatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WechatApplication.class, args);
 	}
+	@Autowired
+	private MessageInfoMapper messageInfoMapper;
 
 	@RequestMapping("hello")
 	@ResponseBody
 	public String hello() {
+
+
+		MessageInfoDto record = new MessageInfoDto();
+		record.setUpdateTime(new Date());
+		messageInfoMapper.insertSelective(record);
 		return "hello world";
 	}
 }
